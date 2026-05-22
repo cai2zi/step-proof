c503e8de9b829fe98438e59de2dd786eeeac4506
cai_nm <zi386002@gmail.com>
2026-05-22 19:50:43 +0800

    10

--- experiments/rollout_rerank_math_verify/scripts/path_env.sh
+++ experiments/rollout_rerank_math_verify/scripts/path_env.sh
@@ -14,3 +14,22 @@ export DATA_ROOT="${DATA_ROOT:-${CZX_ROOT}/data_raw}"
 export MATHLIB_ROOT="${MATHLIB_ROOT:-${CZX_ROOT}/mathlib4}"
 export LEAN_TEMP_ROOT="${LEAN_TEMP_ROOT:-${CZX_ROOT}/czx_work/TEMP/lean_jobs}"
 
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-${CZX_ROOT}/.cache}"
+export VLLM_CACHE_ROOT="${VLLM_CACHE_ROOT:-${XDG_CACHE_HOME}/vllm}"
+export TORCHINDUCTOR_CACHE_DIR="${TORCHINDUCTOR_CACHE_DIR:-${XDG_CACHE_HOME}/torchinductor}"
+export TRITON_CACHE_DIR="${TRITON_CACHE_DIR:-${XDG_CACHE_HOME}/triton}"
+export HF_HOME="${HF_HOME:-${XDG_CACHE_HOME}/huggingface}"
+export TRANSFORMERS_CACHE="${TRANSFORMERS_CACHE:-${HF_HOME}}"
+export TMPDIR="${TMPDIR:-${CZX_ROOT}/TEMP}"
+
+
+export VLLM_NO_USAGE_STATS=1
+export VLLM_DO_NOT_TRACK=1
+export DO_NOT_TRACK=1
+
+mkdir -p "${XDG_CACHE_HOME}"
+mkdir -p "${VLLM_CACHE_ROOT}"
+mkdir -p "${TORCHINDUCTOR_CACHE_DIR}"
+mkdir -p "${TRITON_CACHE_DIR}"
+mkdir -p "${HF_HOME}"
+mkdir -p "${TMPDIR}"
